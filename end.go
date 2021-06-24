5d113e1dab74a90f90d3aea015888c2cc968649e
servermanagementacc <86392894+servermanagementacc@users.noreply.github.com>
2021-06-24 14:42:19 +0800

    Update end.go

--- end.go
+++ end.go
@@ -22,13 +22,13 @@ func endConversationWorker(jobs <-chan EndConversationEvent) {
 			db.Exec("UPDATE users SET match_chat_id = NULL, available = 0, previous_match = ? WHERE chat_id = ?", u.MatchChatID, u.ChatID)
 			db.Exec("UPDATE users SET match_chat_id = NULL, available = 0, previous_match = ? WHERE chat_id = ?", u.ChatID, u.MatchChatID)
 
-			telegram.SendMessage(u.MatchChatID.Int64, "Your chat's ended! Start a new one! :D follow @unichatbotchannel for announcements/updates/ways to support us!", emptyOpts)
+			telegram.SendMessage(u.MatchChatID.Int64, "Your chat's ended! Start a new one! :D", emptyOpts)
 			telegram.SendMessage(u.MatchChatID.Int64, "Type /start to get matched with a new partner", emptyOpts)
 		} else {
 			db.Exec("UPDATE users SET available = 0 WHERE chat_id = ?", u.ChatID)
 		}
 
-		telegram.SendMessage(u.ChatID, "Your conversation is over, I hope you enjoyed it :) follow @unichatbotchannel for announcements/updates/ways to support development!", emptyOpts)
+		telegram.SendMessage(u.ChatID, "Your conversation is over, I hope you enjoyed it :)", emptyOpts)
 		telegram.SendMessage(u.ChatID, "Type /start to get matched with a new partner", emptyOpts)
 	}
 }
